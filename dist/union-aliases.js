0c5319821e69481fa242a022b5f9f4f1c2d45764
Anh Le <haianhltr@yahoo.com.vn>
2022-06-14 00:51:33 +0700

    add-alias

--- dist/union-aliases.js
+++ dist/union-aliases.js
@@ -1,11 +1,12 @@
 "use strict";
 function combine(n1, n2, resultConversion) {
     let result;
-    if (typeof n1 === "number" && typeof n2 === "number" || resultConversion === 'as-number') {
-        return result = +n1 + +n2;
+    if ((typeof n1 === "number" && typeof n2 === "number") ||
+        resultConversion === "as-number") {
+        return (result = +n1 + +n2);
     }
     else {
-        return result = n1.toString() + n2.toString();
+        return (result = n1.toString() + n2.toString());
     }
     //   if (resultConversion === "as-number") {
     //     return +result;
@@ -13,10 +14,10 @@ function combine(n1, n2, resultConversion) {
     //       return result.toString();
     //   }
 }
-const combinedAges = combine(36, 26, "as-number");
+const combinedAges = combine(36, 26, "as-text");
 console.log(combinedAges);
 //literal text
-const combinedStringAges = combine(10, 20, 'as-number');
+const combinedStringAges = combine(10, 20, "as-number");
 console.log(combinedStringAges);
 const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
